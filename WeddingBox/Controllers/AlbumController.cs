abbf77992cec5cd8c81df45c62ca243be22a1363
iverworld <iverworld@yahoo.com.hk>
2014-12-28 15:57:22 +0800

    Updated to a complete version.

--- WeddingBox/Controllers/AlbumController.cs
+++ WeddingBox/Controllers/AlbumController.cs
@@ -6,25 +6,26 @@ using System.Web.Mvc;
 
 namespace WeddingBox.Controllers
 {
-    public class AlbumViewModel
-    {
-        #region Fields
-        public List<string> filePaths { get; set; }
-        #endregion
-    }
-
     public class AlbumController : Controller
     {
         // GET: Album
         public ActionResult Index()
         {
-            var fileList = System.IO.Directory.GetFiles(Server.MapPath("~/Album/")).Select(x =>
+            var fileList = System.IO.Directory.GetFiles(Server.MapPath("~/Resource/Prewedding")).Select(x =>
             {
                 var index = x.LastIndexOf("\\");
-                return x.Substring(index + 1, x.Length - index - 1);
+                return "/Resource/Prewedding/" + x.Substring(index + 1, x.Length - index - 1);
             }).ToList();
             var viewModel = new AlbumViewModel { filePaths = fileList };
             return View(viewModel);
+        }
+
+
+        public class AlbumViewModel
+        {
+            #region Fields
+            public List<string> filePaths { get; set; }
+            #endregion
         }
     }
 }
